788446435a623c41ec73e58eea58b296f9141d08
ataotao <tzvuf@163.com>
2020-04-10 16:44:09 +0800

    update

--- 14.最长公共前缀.js
+++ 14.最长公共前缀.js
@@ -10,6 +10,7 @@
  * @return {string}
  */
 const longestCommonPrefix = function (strs) {
+    if(!strs.length) return '';
     let res = '';
     // 取得位数最短的字符
     const min = Math.min.apply(null, strs.map((v) => v.length));
@@ -27,5 +28,19 @@ const longestCommonPrefix = function (strs) {
     }
     return res;
 };
+
+
+// var longestCommonPrefix = function(strs) {
+//     if(!strs.length) return '';
+//     let [a, ...b] = strs;
+//     let result = '';
+//     for(let i = 0; i < a.length; i++){
+//         let flag = b.every(item => item[i] === a[i]);
+//         if(flag) result += a[i];
+//         else break;
+//     }
+//     return result;
+// };
+
 
 // @lc code=end
